5431405331affba873365ca6a051467e295e8269
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-01-05 09:12:29 -0300

    Start

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <div className='bg-gray-50 text-gray-700'>
+        <div className='container mx-auto transform duration-500'>
+          <div id='home' className='pl-4 pr-4 pt-5 pb-10'>
+            <div className='flex justify-between items-center'>
+              <div>
+                list logo
+              </div>
+              <div>
+                Home Menu Product Blogs About
+              </div>
+              <div>
+                icon01 icon02 icon03
+              </div>
+            </div>
+          </div>
+          <div id='hero'></div>
+          <div id='menu' className='pl-5 pr-5'></div>
+          <div id='product' className='pl-5 pr-5'></div>
+          <div id='blog' className='pl-5 pr-5'></div>
+          <div id='about' className='pl-5 pr-5'></div>
+        </div>
+      </div>
     </div>
   );
 }
